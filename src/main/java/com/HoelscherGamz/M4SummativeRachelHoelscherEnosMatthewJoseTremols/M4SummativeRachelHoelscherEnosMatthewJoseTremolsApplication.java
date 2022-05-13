3733322766b454da735a1a092d48efa35a2422f2
NukaGrizz <nukagrizz@gmail.com>
2022-05-13 17:21:49 -0400

    Created Project Using Spring Initializer

---
+++ src/main/java/com/HoelscherGamz/M4SummativeRachelHoelscherEnosMatthewJoseTremols/M4SummativeRachelHoelscherEnosMatthewJoseTremolsApplication.java
@@ -0,0 +1,13 @@
+package com.HoelscherGamz.M4SummativeRachelHoelscherEnosMatthewJoseTremols;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M4SummativeRachelHoelscherEnosMatthewJoseTremolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M4SummativeRachelHoelscherEnosMatthewJoseTremolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HoelscherGamz/M4SummativeRachelHoelscherEnosMatthewJoseTremols/M4SummativeRachelHoelscherEnosMatthewJoseTremolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HoelscherGamz.M4SummativeRachelHoelscherEnosMatthewJoseTremols;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M4SummativeRachelHoelscherEnosMatthewJoseTremolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
