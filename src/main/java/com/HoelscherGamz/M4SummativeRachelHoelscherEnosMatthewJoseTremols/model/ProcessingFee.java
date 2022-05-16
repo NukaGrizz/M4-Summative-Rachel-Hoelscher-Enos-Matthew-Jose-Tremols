9cdd92fee92f8b26621129dc3778e9fd110a49e2
me0wmerz <rachel.hoelscher@gmail.com>
2022-05-16 08:01:06 -0700

    set up sales tax table

---
+++ src/main/java/com/HoelscherGamz/M4SummativeRachelHoelscherEnosMatthewJoseTremols/model/ProcessingFee.java
@@ -0,0 +1,12 @@
+package com.HoelscherGamz.M4SummativeRachelHoelscherEnosMatthewJoseTremols.model;
+
+public class ProcessingFee {
+
+    /*
+    Consoles: 14.99
+
+T-shirts: 1.98
+
+Games: 1.49
+     */
+}
