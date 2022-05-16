44ba65eddf7c6b1ff8e8bab473521b0ce91ba6e8
me0wmerz <rachel.hoelscher@gmail.com>
2022-05-16 09:15:39 -0700

    added getters and setters and constructors etc

--- src/main/java/com/HoelscherGamz/M4SummativeRachelHoelscherEnosMatthewJoseTremols/model/ProcessingFee.java
+++ src/main/java/com/HoelscherGamz/M4SummativeRachelHoelscherEnosMatthewJoseTremols/model/ProcessingFee.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
@@ -20,4 +21,45 @@ public class ProcessingFee {
     @Digits(integer = 4,fraction = 2)
     private BigDecimal fee;
 
+    public String getProduct_type() {
+        return product_type;
+    }
+
+    public void setProduct_type(String product_type) {
+        this.product_type = product_type;
+    }
+
+    public BigDecimal getFee() {
+        return fee;
+    }
+
+    public void setFee(BigDecimal fee) {
+        this.fee = fee;
+    }
+
+    public ProcessingFee(String product_type, BigDecimal fee) {
+        this.product_type = product_type;
+        this.fee = fee;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProcessingFee that = (ProcessingFee) o;
+        return Objects.equals(product_type, that.product_type) && Objects.equals(fee, that.fee);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(product_type, fee);
+    }
+
+    @Override
+    public String toString() {
+        return "ProcessingFee{" +
+                "product_type='" + product_type + '\'' +
+                ", fee=" + fee +
+                '}';
+    }
 }
